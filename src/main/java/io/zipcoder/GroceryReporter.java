8287e0b8ca087ec506563502da255a4b6e2f9501
Kavya Muppalla <kavyamuppalla@gmail.com>
2019-07-10 12:10:35 -0400

    tostring pending

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,6 +1,12 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GroceryReporter {
     private final String originalFileText;
@@ -11,6 +17,49 @@ public class GroceryReporter {
 
     @Override
     public String toString() {
-        return null;
+
+        ItemParser itemParser = new ItemParser();
+        List<Item> items = itemParser.parseItemList(this.originalFileText);
+
+        Map<String, Map<String, Integer>> itemMap = new LinkedHashMap<>();
+        int errorCount = 0;
+        for(Item item: items) {
+
+            if(item.getName() == null) {
+                errorCount++;
+            } else {
+                Map<String, Integer> countMap = itemMap.containsKey(item.getName()) ? itemMap.get(item.getName()) : new LinkedHashMap<>();
+                int itemCount = countMap.containsKey("ITEM_COUNT") ? countMap.get("ITEM_COUNT") : 0;
+
+                if (item.getPrice() != null) {
+                    int priceCount = countMap.containsKey(item.getPrice().toString()) ? countMap.get(item.getPrice().toString()) : 0;
+                    countMap.put(item.getPrice().toString(), ++priceCount);
+                    countMap.put("ITEM_COUNT", ++itemCount);
+                } else {
+                    errorCount++;
+                }
+
+                itemMap.put(item.getName(), countMap);
+            }
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(String key : itemMap.keySet()) {
+            sb.append("name:\t" + key.substring(0, 1).toUpperCase() + key.substring(1) +" \t\t seen: " + itemMap.get(key).get("ITEM_COUNT") + " times");
+            sb.append("\n=================\n");
+            itemMap.get(key).remove("ITEM_COUNT");
+            for(String price: itemMap.get(key).keySet()) {
+                sb.append("Price:\t" + price + "\t\t seen: " + itemMap.get(key).get(price) + " times");
+                sb.append("\n---------------\n");
+            }
+
+        }
+
+        if (errorCount > 0) {
+            sb.append("Errors \t seen: " + errorCount + " times");
+        }
+
+        return sb.toString();
     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,69 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class ItemParser {
+
+    private int errorCount = 0;
+
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+
+        List<Item> items = new ArrayList<>();
+        Pattern itemPattern = Pattern.compile("##", Pattern.CASE_INSENSITIVE);
+        String[] itemStrings = itemPattern.split(valueToParse);
+
+        for(String itemString : itemStrings) {
+            try {
+                items.add(parseSingleItem(itemString+"##"));
+            } catch (ItemParseException itemParseException) {
+                System.out.println("Failed to parse === " + itemString);
+                itemParseException.printStackTrace();
+                errorCount++;
+            }
+        }
+
+        return items;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+
+        String name = null;
+        String type = null;
+        String expiration = null;
+        Double price = null;
+
+        Pattern fieldPattern = Pattern.compile(";");
+        Pattern nameValuePattern = Pattern.compile("[@:\\^*%]");
+        singleItem = singleItem.substring(0, singleItem.length()-2);
+
+        String[] fieldStrings = fieldPattern.split(singleItem);
+        for(String fieldString: fieldStrings) {
+            String[] nameValues = nameValuePattern.split(fieldString);
+            if (nameValues[0].equalsIgnoreCase("name")) {
+                name = (nameValues.length > 1 && nameValues[1] != null) ? nameValues[1].toLowerCase() : null;
+            }
+            else if (nameValues[0].equalsIgnoreCase("type")) {
+                type = (nameValues.length > 1 && nameValues[1] != null) ? nameValues[1].toLowerCase() : null;
+            }
+            else if (nameValues[0].equalsIgnoreCase("expiration")) {
+                expiration = (nameValues.length > 1 && nameValues[1] != null) ? nameValues[1].toLowerCase() : null;
+            }
+            else if (nameValues[0].equalsIgnoreCase("price")) {
+                price = (nameValues.length > 1 && nameValues[1] != null) ? Double.parseDouble(nameValues[1]) : null;
+            }
+        }
+        if (name == null || type == null || expiration == null || price == null) {
+            throw new ItemParseException();
+        }
+        return new Item(name, price, type, expiration);
+
+    }
+
+    public int getErrorCount() {
+        return errorCount;
     }
 }
